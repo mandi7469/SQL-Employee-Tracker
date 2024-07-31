78874084b2711550857b1b067bb5ae6e3293d1f2
mandi7469 <mandi7469@aol.com>
2024-07-31 17:27:49 -0400

    added database and tables to schema file

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+-- drops database if exists and creates the database with the given name
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+-- connects to the database 
+\c employee_tracker_db
+
+-- creates table for department with id and name columns and primary key
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id) ON DELETE CASCADE
+);
+
+-- creates table for employee with id, first_name, last_name, role_id, and manager_id columns and primary and foreign key 
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+      
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
