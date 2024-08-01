854747bf16f0430ea09a2a3048357d34a1a58d57
mandi7469 <mandi7469@aol.com>
2024-07-31 21:44:38 -0400

    completed find departments and find roles query functions

--- db/index.js
+++ db/index.js
@@ -13,11 +13,22 @@ class DB {
     }
   }
 
+   // find departments
+   findDepartments() {
+    return this.query('SELECT department.id, department.name FROM department;');
+  }
 
-
-
-
+ // find all roles, joined with the department that role belongs to and the salary for that role
+ findRoles() {
+  return this.query(
+    'SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;'
+  );
+}
 
 
 
 }
+
+
+
+module.exports = new DB();
